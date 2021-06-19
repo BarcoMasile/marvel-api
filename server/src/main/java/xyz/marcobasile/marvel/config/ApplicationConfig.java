624b46876c58baa9ef4fa2fd0c6005ed84c1d268
Marco Basile <mbasile@tai.it>
2021-06-19 17:57:55 +0200

    removed useless file

--- server/src/main/java/xyz/marcobasile/marvel/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package xyz.marcobasile.marvel.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(MarvelProperties.class)
-public class ApplicationConfig {
-}
